58c7b1fcc3a9aebdaf4bba5d328e1650e7f240a8
Nland22 <nland22@gmail.com>
2012-11-21 16:24:10 -0500

    Initial Commit

---
+++ Diamond/src/Diamond.java
@@ -0,0 +1,28 @@
+
+public class Diamond {
+
+	/**
+	 * Nicholas Land
+	 * CSE 230 
+	 * 9-13-12
+	 * Project 1
+	 */
+	public static void main(String[] args) {
+		
+		/* Here are the print statements
+		 * for creating the diamond figure
+		 */
+		
+		System.out.println("    *    ");
+		System.out.println("   ***   ");
+		System.out.println("  *****  ");
+		System.out.println(" ******* ");
+		System.out.println("*********");
+		System.out.println(" ******* ");
+		System.out.println("  *****  ");
+		System.out.println("   ***   ");
+		System.out.println("    *    ");
+
+	}
+
+}
