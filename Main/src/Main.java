bf99621cf967b82dce47cdad6257e893f18fae22
Nland22 <nland22@gmail.com>
2012-11-21 16:28:50 -0500

    Initial Commit

---
+++ Main/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	/**
+	 * Nicholas Land
+	 * CSE 230 
+	 * 9-13-12
+	 * Project 1
+	 */
+	
+	public Main() {
+		
+	}
+	public static void main(String[] args) {
+		
+		// These are print statements from the sample program
+		
+		System.out.println("Java");
+		System.out.println("Programming Language");
+	}
+
+}
