200b1bd93557fa5a68177b61f280bcd8fd6321ad
Nick <nland22@gmail.com>
2012-11-23 12:34:59 -0500

    Initial commit of Dog program Head first Java

---
+++ Dog/src/DogTestDrive.java
@@ -0,0 +1,15 @@
+
+public class DogTestDrive {
+
+	public static void main(String[] args) {
+		
+		// Make a new dog object
+		Dog d = new Dog();
+		
+		// set size of dog using dot operator
+		// and call the the bark method on the
+		// dog
+		d.size = 40; 
+		d.bark();
+	}
+}
